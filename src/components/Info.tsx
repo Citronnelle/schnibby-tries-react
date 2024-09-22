45c1ec527580132b7841f2c4092d47201e5ffb2c
Taiki-Viia Tungal <taikiviia@gmail.com>
2024-09-22 23:43:08 +0300

    feat: utilizing Grid v2 in the layout of the Info component

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, FormEvent, useState } from "react"
-import { Box, Button, List, ListItem, TextField } from "@mui/material"
+import { Box, Button, Grid2, List, ListItem, TextField } from "@mui/material"
 
 const huviHobiMassiiv = [
   "Dungeons & Dragons",
@@ -29,33 +29,16 @@ const Info = ({ nimi }: { nimi: string }) => {
 
   return (
     <>
-      <Box sx={{ fontSize: "calc(10px + 2vmin)" }}>{nimi}</Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "space-evenly",
-        }}
+      <Grid2
+        container
+        spacing={{ xs: 1, sm: 2, md: 3 }}
       >
-        <List
-          sx={{
-            display: "flex",
-            alignItems: "flex-start",
-            flexDirection: "column",
-            margin: "5px",
-            padding: "5px",
-            height: "100%",
-          }}
-        >
-          {huviHobiMassiiv.map(huviHobi => (
-            <ListItem key={huviHobi}>{huviHobi}</ListItem>
-          ))}
-        </List>
+        <Grid2 size={{ xs: 12 }}>
+          <Box sx={{ fontSize: "calc(10px + 2vmin)" }}>{nimi}</Box>
+        </Grid2>
 
-        <form onSubmit={tootleSaatmist}>
-          <Box
+        <Grid2 size={{ xs: 12, sm: 6 }}>
+          <List
             sx={{
               display: "flex",
               alignItems: "flex-start",
@@ -65,37 +48,55 @@ const Info = ({ nimi }: { nimi: string }) => {
               height: "100%",
             }}
           >
-            <TextField
-              sx={{ marginBottom: "10px" }}
-              id="email"
-              name="email"
-              label="E-posti aadress:"
-              type="email"
-              variant="outlined"
-              fullWidth
-              onChange={tootleMuutust}
-            />
-            <TextField
-              sx={{ marginBottom: "10px" }}
-              id="teade"
-              name="teade"
-              label="Kirja sisu:"
-              multiline
-              rows={4}
-              variant="outlined"
-              fullWidth
-              onChange={tootleMuutust}
-            />
-            <Button
-              sx={{ alignSelf: "flex-end" }}
-              type="submit"
-              variant="contained"
+            {huviHobiMassiiv.map(huviHobi => (
+              <ListItem key={huviHobi}>{huviHobi}</ListItem>
+            ))}
+          </List>
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6 }}>
+          <form onSubmit={tootleSaatmist}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "flex-start",
+                flexDirection: "column",
+                margin: "5px",
+                padding: "5px",
+                height: "100%",
+              }}
             >
-              Saada!
-            </Button>
-          </Box>
-        </form>
-      </Box>
+              <TextField
+                sx={{ marginBottom: "10px" }}
+                id="email"
+                name="email"
+                label="E-posti aadress:"
+                type="email"
+                variant="outlined"
+                fullWidth
+                onChange={tootleMuutust}
+              />
+              <TextField
+                sx={{ marginBottom: "10px" }}
+                id="teade"
+                name="teade"
+                label="Kirja sisu:"
+                multiline
+                rows={4}
+                variant="outlined"
+                fullWidth
+                onChange={tootleMuutust}
+              />
+              <Button
+                sx={{ alignSelf: "flex-end" }}
+                type="submit"
+                variant="contained"
+              >
+                Saada!
+              </Button>
+            </Box>
+          </form>
+        </Grid2>
+      </Grid2>
     </>
   )
 }
